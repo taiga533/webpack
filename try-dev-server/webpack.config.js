3d3df00d9c7a0f78dc6f937f33527e71e982fd6e
川﨑 大河 <kawasaki.taiga@raccoon.ne.jp>
2020-03-18 16:09:30 +0900

    dev-server周り修正2

--- try-dev-server/webpack.config.js
+++ try-dev-server/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     mode: "development",
     entry: {
-        main: path.resolve(__dirname, 'src', 'index.js')
+        index: path.resolve(__dirname, 'src', 'index.js')
     },
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
