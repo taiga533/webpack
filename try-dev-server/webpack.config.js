306eaa555202a5e9f1b5868a74b130e11440b328
川﨑 大河 <kawasaki.taiga@raccoon.ne.jp>
2020-03-18 13:24:24 +0900

    dev-serverの模範解答追加

--- try-dev-server/webpack.config.js
+++ try-dev-server/webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: "development",
     entry: {
-        index: path.resolve(__dirname, 'src', 'index.js')
+        main: path.resolve(__dirname, 'src', 'index.js')
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].bundle.js'
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [new HtmlWebpackPlugin()]
 }
