e6f28b3a2c38653c744917aae08be0d568f83638
川﨑 大河 <kawasaki.taiga@raccoon.ne.jp>
2020-03-25 16:21:27 +0900

    try-babelの修正

--- try-babel/webpack.config.js
+++ try-babel/webpack.config.js
@@ -2,40 +2,18 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: // introductionと同じ設定を書く,
+    mode: "development",
     entry: {
-        // introductionと同じ設定を書く
+        index: path.resolve(__dirname, 'src', 'index.js')
     },
     module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: [
-                                ["@babel/preset-env", {
-                                useBuiltIns: "usage",
-                                corejs: 3,
-                                targets: {
-                                    browsers: [
-                                    "last 1 Chrome version",
-                                    "last 1 Firefox version",
-                                    "last 1 Edge version",
-                                    "ie 11",
-                                    "last 1 ChromeAndroid version",
-                                    "last 2 iOS versions",
-                                    "last 2 Safari versions",
-                                ]
-                                },
-                            }]
-                            ]
-                        }
-                    }
-                ]
-            }
-        ]
+        rules: []
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [new HtmlWebpackPlugin()]
+    
 }
